914de5aedfee2c068884af571c6b0f17c3713950
IVIanuu <IVIanuu@gmail.com>
2018-09-27 18:37:29 +0200

    maven local

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -6,10 +6,11 @@ object Versions {
     const val minSdk = 14
     const val targetSdk = 28
     const val versionCode = 1
-    const val versionName = "1.0"
+    const val versionName = "SNAPSHOT"
+    const val groupId = "com.ivianuu.androidktx"
 
-    const val androidGradlePlugin = "3.3.0-alpha10"
-    const val androidx = "1.0.0-rc02"
+    const val androidGradlePlugin = "3.2.0"
+    const val androidx = "1.0.0"
     const val androidxArch = "2.0.0-rc01"
     const val archWork = "1.0.0-alpha08"
     const val constraintLayout = "1.1.3"
@@ -21,22 +22,16 @@ object Deps {
     const val androidGradlePlugin = "com.android.tools.build:gradle:${Versions.androidGradlePlugin}"
 
     const val androidXAnnotation = "androidx.annotation:annotation:${Versions.androidx}"
-
     const val androidxAppCompat = "androidx.appcompat:appcompat:${Versions.androidx}"
-
     const val androidxCardView = "androidx.cardview:cardview:${Versions.androidx}"
-
     const val androidxCore = "androidx.core:core:${Versions.androidx}"
     const val androidxCoreKtx = "androidx.core:core-ktx:${Versions.androidx}"
-
     const val androidxFragment = "androidx.fragment:fragment:${Versions.androidx}"
+    const val androidxRecyclerView = "androidx.recyclerview:recyclerview:${Versions.androidx}"
+    const val androidxViewPager = "androidx.viewpager:viewpager:${Versions.androidx}"
 
     const val archLifecycleExtensions =
         "androidx.lifecycle:lifecycle-extensions:${Versions.androidxArch}"
-
-    const val androidxRecyclerView = "androidx.recyclerview:recyclerview:${Versions.androidx}"
-
-    const val androidxViewPager = "androidx.viewpager:viewpager:${Versions.androidx}"
 
     const val kotlinGradlePlugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
     const val kotlinStdLib = "org.jetbrains.kotlin:kotlin-stdlib-jdk7:${Versions.kotlin}"
