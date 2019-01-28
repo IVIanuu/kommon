26257c24726dd0676c1d57ba94393a0deb09aeaa
IVIanuu <IVIanuu@gmail.com>
2019-01-28 16:12:33 +0100

    Do not use git hashes for versioning

--- buildSrc/src/main/java/dependencies.kt
+++ buildSrc/src/main/java/dependencies.kt
@@ -14,7 +14,7 @@ object Build {
 object Publishing {
     const val groupId = "com.ivianuu.kommon"
     const val vcsUrl = "https://github.com/IVIanuu/kommon"
-    const val version = "git"
+    const val version = "${Build.versionName}-dev-1"
 }
 
 object Versions {
